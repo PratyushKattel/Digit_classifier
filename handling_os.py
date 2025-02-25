0250fdca0b7f5e8303a2fc460d0e707377495423
PratyushKattel <pratyushkatel@gmail.com>
2025-02-25 15:34:19 +0545

    sorting the files

--- handling_os.py
+++ handling_os.py
@@ -9,13 +9,15 @@ for folder in folders[1:]:
         digits=os.listdir()
         for digit in digits:
             os.chdir(digit)
-            for index,image in enumerate(os.listdir(),1):
+            for index,image in enumerate(os.listdir(),0):
                 new_name=str(index)+'.png'
                 old_path=os.path.join(os.getcwd(),image)
                 new_path=os.path.join(os.getcwd(),new_name)
                 os.rename(old_path,new_path)
                 print(f"{image} is renamed to {new_name}")
             os.chdir('..')
+
+        os.chdir('..')
 
     os.chdir(folder)
     images=os.listdir()
